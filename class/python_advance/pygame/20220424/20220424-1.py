2430575b3e4e6cb1bccadebc1a6841c509acee19
RyanSu1234 <ryan.yufan.su@gmail.com>
2022-04-24 11:05:31 +0800

    20220424

--- class/python_advance/pygame/20220424/20220424-1.py
+++ class/python_advance/pygame/20220424/20220424-1.py
@@ -14,6 +14,7 @@ LIMIT_HIGH = 140
 pygame.init()
 timer = 0
 clock = pygame.time.Clock()
+RED = (255, 0, 0)
 #***初始化設定結束***
 
 #===載入圖片開始===
@@ -23,6 +24,7 @@ img_dinosaur = [
     pygame.image.load("小恐龍2.png"),
 ]
 img_cacti = pygame.image.load('cacti.png')
+img_gg = pygame.image.load('gameover.png')
 #***載入圖片結束***
 
 #===遊戲視窗設定開始===
@@ -36,6 +38,17 @@ screen = pygame.display.set_mode(bg_size)
 #***遊戲視窗設定結束***
 
 #===分數設定開始===
+score = 0
+typefase = pygame.font.get_default_font()
+score_font = pygame.font.Font(typefase, 36)
+
+
+def get_score(win):
+    global score
+
+    score_sur = score_font.render(str(score), True, RED)
+    win.blit(score_sur, [10, 10])
+
 
 #***分數設定結束***
 
@@ -58,30 +71,54 @@ def move_dinosaur(win, timer):
 
         if ds_y >= LIMIT_LOW:
             jumpState = False
-    win.blit(img_dinosaur[timer % len(img_dinosaur)], [ds_x, ds_y])
+    win.blit(img_dinosaur[timer % 20 // 10], [ds_x, ds_y])
 
 
 #***恐龍設定結束***
 
 #===仙人掌設定開始===
+casti_w = img_cacti.get_width()
+casti_h = img_cacti.get_height()
 cacti_x = bg_x - 10
 cacti_y = LIMIT_LOW
 cacti_shift = 10
+cacti_dist = int((casti_w + casti_h) / 2)
 
 
 def move_cacti(win):
     global cacti_x, cacti_y, cacti_shift
-    cacti_x = (cacti_x - cacti_shift) % bg_x
-    screen.blit(img_cacti, [cacti_x, cacti_y])
+    global score
+
+    cacti_x -= cacti_shift
+    if (cacti_x < 0):
+        score += 1
+        cacti_x = bg_x - 100
+        cacti_shift = 10
+    win.blit(img_cacti, [cacti_x, cacti_y])
 
 
 #***仙人掌設定結束***
 
+
 #***碰撞設定結束***
+def is_hit(x1, y1, x2, y2, r):
+    if ((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2)) < (r * r):
+        return True
+    else:
+        return False
+
 
 #***碰撞設定結束***
 
 #===GameOver設定開始===
+gg = False
+gg_w = img_gg.get_width()
+gg_h = img_gg.get_height()
+
+
+def game_over(win):
+    win.blit(img_gg, ((bg_x - gg_w) / 2, (bg_y - gg_h) / 2))
+
 
 #***GameOver設定結束***
 
@@ -97,16 +134,28 @@ while True:
         if event.type == KEYDOWN:
             if event.key == K_SPACE and ds_y <= LIMIT_LOW:
                 jumpState = True
+            elif event.key == K_RETURN and gg == True:
+                gg = False
+                cacti_x = bg_x - 100
+                ds_x = 50
+                ds_y = LIMIT_LOW
+                score = 0
+                jumpState = False
 
-    #===遊戲結束===
+    if (gg == True):
+        #===遊戲結束===
+        game_over(screen)
+    else:
+        #===遊戲進行===
+        roll_x = (roll_x - 10) % bg_x
+        screen.blit(img, [roll_x - bg_x, 0])
+        screen.blit(img, [roll_x, 0])
+        #===更新角色狀態===
+        move_dinosaur(screen, timer)
+        move_cacti(screen)
+        get_score(screen)
+        if (is_hit(ds_x, ds_y, cacti_x, cacti_y, cacti_dist)):
+            gg = True
 
-    #===遊戲進行===
-    roll_x = (roll_x - 10) % bg_x
-    print(roll_x)
-    screen.blit(img, [roll_x - bg_x, 0])
-    screen.blit(img, [roll_x, 0])
-    #===更新角色狀態===
-    move_dinosaur(screen, timer)
-    move_cacti(screen)
     pygame.display.update()
 #===主程式結束===
